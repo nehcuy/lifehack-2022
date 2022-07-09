b48764b33be21b6af0879b57da4ec0ade75462ba
kxrt <kvrtikeya@gmail.com>
2022-07-09 19:22:10 +0800

    Initialise MobileLanding

---
+++ src/pages/MobileLanding.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import Header from "../components/Header";
+
+const MobileLanding = () => {
+  return (
+    <>
+      <div className="App">
+          <Header />
+      </div>
+    </>
+  );
+}
+
+export default MobileLanding;
