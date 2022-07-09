ddb2a73b9ef329e94359e327825d71cdd5ccfe3f
kxrt <kvrtikeya@gmail.com>
2022-07-10 01:04:53 +0800

    Upload code on submit

--- src/pages/MobileLanding.js
+++ src/pages/MobileLanding.js
@@ -10,14 +10,18 @@ import url from "../utils/url";
 
 const MobileLanding = () => {
   const [loading, setLoading] = React.useState(false);
-  const onSubmit = async () => {
+  const onSubmit = async (e) => {
+    e.preventDefault();
     setLoading(true);
-    const response = await fetch(url + "/connect/newPhone/1234", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await fetch(
+      url + "/connect/newPhone/" + e.target[0].value,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     const data = await response.json(); // data is the new phone object
     localStorage.setItem("phone", JSON.stringify(data));
     window.location.href = "/mobile-auth";
@@ -39,43 +43,44 @@ const MobileLanding = () => {
         >
           Please enter your authentication code to continue.
         </Typography>
-        <TextField
-          type="number"
-          id="quantity"
-          label="Code"
-          inputProps={{ inputMode: "numeric" }}
-        />
-        <div>
-          <Button
-            disableElevation
-            type="submit"
-            variant="contained"
-            color="primary"
-            onClick={onGoBack}
-            sx={{
-              backgroundColor: "#8c9ba5",
-              color: "white",
-              margin: "1vh",
-            }}
-          >
-            Back
-          </Button>
-          <Button
-            disableElevation
-            type="submit"
-            variant="contained"
-            color="primary"
-            disabled={loading}
-            onClick={onSubmit}
-            sx={{
-              backgroundColor: "#5596e6",
-              color: "white",
-              margin: "1vh",
-            }}
-          >
-            {(!loading && "Submit") || <CircularProgress size={20} />}
-          </Button>
-        </div>
+        <form onSubmit={onSubmit}>
+          <TextField
+            type="number"
+            id="quantity"
+            label="Code"
+            inputProps={{ inputMode: "numeric" }}
+          />
+          <div>
+            <Button
+              disableElevation
+              type="submit"
+              variant="contained"
+              color="primary"
+              onClick={onGoBack}
+              sx={{
+                backgroundColor: "#8c9ba5",
+                color: "white",
+                margin: "1vh",
+              }}
+            >
+              Back
+            </Button>
+            <Button
+              disableElevation
+              type="submit"
+              variant="contained"
+              color="primary"
+              disabled={loading}
+              sx={{
+                backgroundColor: "#5596e6",
+                color: "white",
+                margin: "1vh",
+              }}
+            >
+              {(!loading && "Submit") || <CircularProgress size={20} />}
+            </Button>
+          </div>
+        </form>
       </Box>
     </>
   );
