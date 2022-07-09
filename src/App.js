f2770d01c13518faa516605446c7bb23a5915d79
Yuchen <hyuchen2607@gmail.com>
2022-07-09 19:21:26 +0800

    Initialise LandingPage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import Header from "./components/Header";
 import LockButton from "./components/LockButton";
+import LaptopLanding from "./components/LaptopLanding";
+import Starting from "./components/Starting";
 
 function App() {
   return (
     <>
-      <div className="App">
+      <Starting />
+      {/* <div className="App">
           <Header />
           <LockButton />
-      </div>
+      </div> */}
     </>
   );
 }
